6d79338cc647c9c30a4dee58f3dd49eb5791738d
DTPriyankara <tharijupriyankara@gmail.com>
2018-09-23 08:10:44 +0530

    after the all admin panel creations

---
+++ app/src/main/java/com/example/dushyantha/attendanceapp/CourseAdd.java
@@ -0,0 +1,17 @@
+package com.example.dushyantha.attendanceapp;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CourseAdd extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.course_add, container, false);
+        return rootView;
+    }
+}
