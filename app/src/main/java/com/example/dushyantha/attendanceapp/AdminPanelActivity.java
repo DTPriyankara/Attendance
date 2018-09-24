14707057b51e3b08a3ccfc791f8cd9ca82f2ac59
DTPriyankara <tharijupriyankara@gmail.com>
2018-09-24 16:57:30 +0530

    Modify the admin panel change colors

--- app/src/main/java/com/example/dushyantha/attendanceapp/AdminPanelActivity.java
+++ app/src/main/java/com/example/dushyantha/attendanceapp/AdminPanelActivity.java
@@ -1,8 +1,10 @@
 package com.example.dushyantha.attendanceapp;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,6 +15,8 @@ public class AdminPanelActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.admin_panel);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         //button Student===============================================
         button = (Button) findViewById(R.id.btn_student);
@@ -41,8 +45,15 @@ public class AdminPanelActivity extends AppCompatActivity {
             }
         });
 
-    }
 
+        ActionBar ab = getSupportActionBar();
+        ab.setLogo(R.drawable.ic_action_name);
+        ab.setDisplayUseLogoEnabled(true);
+        ab.setDisplayShowHomeEnabled(true);
+
+
+
+    }
     public void openAdminPanelStudent() {
         Intent intent = new Intent(this, AdminPanelStudentActivity.class);
         startActivity(intent);
@@ -61,4 +72,5 @@ public class AdminPanelActivity extends AppCompatActivity {
     public void onBackPressed(){
 
     }
+
 }
--- app/src/main/java/com/example/dushyantha/attendanceapp/LoginMain.java
+++ app/src/main/java/com/example/dushyantha/attendanceapp/LoginMain.java
@@ -1,13 +1,30 @@
 package com.example.dushyantha.attendanceapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginMain extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_main);
+
+
+        button = (Button) findViewById(R.id.btn_login);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openAdminPanel();
+            }
+        });
+
+    }
+    public void openAdminPanel() {
+        Intent intent = new Intent(this, AdminPanelActivity.class);
+        startActivity(intent);
     }
 }
